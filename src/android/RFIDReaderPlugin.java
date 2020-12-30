9866cd437683fc9e64517cac2e81fdf372cc897c
Alexandre Ogrodovski <alexandreogrodovski@gmail.com>
2020-12-30 18:15:23 -0300

    removi o conteúdo do execute no .java

--- src/android/RFIDReaderPlugin.java
+++ src/android/RFIDReaderPlugin.java
@@ -14,37 +14,9 @@ import org.json.JSONObject;
 public class RFIDReaderPlugin extends CordovaPlugin {
 private static final String DURATION_LONG = "long";
 
-@Override
-public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
+  @Override
+  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
 
-  // Verify that the user sent a 'show' action
-  if (!action.equals("show")) {
-    callbackContext.error("\"" + action + "\" is not a recognized action.");
-    return false;
-  }
-
-  String message;
-  String duration;
-
-  try {
-    JSONObject options = args.getJSONObject(0);
-    message = options.getString("message");
-    duration = options.getString("duration");
-  } catch (JSONException e) {
-    callbackContext.error("Error encountered: " + e.getMessage());
-    return false;
-  }
-
-  // Create the toast
-  Toast toast = Toast.makeText(cordova.getActivity(), message,
-  DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
-
-  // Display toast
-  toast.show();
-
-  // Send a positive result to the callbackContext
-  PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
-  callbackContext.sendPluginResult(pluginResult);
-  return true;
+    return true;
   }
 }
