fabb123f050db8aa31ca4373fc7edde31f9e835a
Alexandre Ogrodovski <alexandreogrodovski@gmail.com>
2021-01-04 08:25:43 -0300

    Formatação

--- www/rfidplugin.js
+++ www/rfidplugin.js
@@ -16,102 +16,81 @@ cordova.addConstructor(RFIDReaderPlugin.install);
 var exec = require('cordova/exec');
 
 RFIDReaderPlugin.prototype.start = function (successCallback, errorCallback) {
-
-	if (errorCallback == null) {
-        errorCallback = function () {
-        };
-    }
-
-    if (typeof errorCallback != "function") {
-        console.log("RFIDReaderPlugin.start: errorCallback não é uma função");
-        return;
-    }
-
-    if (typeof successCallback != "function") {
-        console.log("RFIDReaderPlugin.start: successCallback não é uma função");
-        return;
-    }
-
-	var options = {};
-
-	exec(
-	     	function(result) {
-	            successCallback(result);
-	        },
-            function(error) {
-               errorCallback(error);
-            },
-
-            'RFIDReaderPlugin',
-            'start',
-			[options]
-     );
+  if (errorCallback == null) {
+    errorCallback = function () {};
+  }
+  if (typeof errorCallback != "function") {
+    console.log("RFIDReaderPlugin.start: errorCallback não é uma função");
+    return;
+  }
+  if (typeof successCallback != "function") {
+    console.log("RFIDReaderPlugin.start: successCallback não é uma função");
+    return;
+  }
+  var options = {};
+  exec(
+    function(result) {
+      successCallback(result);
+    },
+    function(error) {
+      errorCallback(error);
+    },
+    'RFIDReaderPlugin',
+    'start',
+    [options]
+  );
 }
 
 RFIDReaderPlugin.prototype.stop = function (successCallback, errorCallback) {
-
-	if (errorCallback == null) {
-        errorCallback = function () {
-        };
-    }
-
-    if (typeof errorCallback != "function") {
-        console.log("RFIDReaderPlugin.stop: errorCallback não é uma função");
-        return;
-    }
-
-    if (typeof successCallback != "function") {
-        console.log("RFIDReaderPlugin.stop: successCallback não é uma função");
-        return;
-    }
-
-	var options = {};
-
-	exec(
-	     	function(result) {
-	            successCallback(result);
-	        },
-            function(error) {
-               errorCallback(error);
-            },
-
-            'RFIDReaderPlugin',
-            'stop',
-			[options]
-     );
+  if (errorCallback == null) {
+    errorCallback = function () {};
+  }
+  if (typeof errorCallback != "function") {
+    console.log("RFIDReaderPlugin.stop: errorCallback não é uma função");
+    return;
+  }
+  if (typeof successCallback != "function") {
+    console.log("RFIDReaderPlugin.stop: successCallback não é uma função");
+    return;
+  }
+  var options = {};
+  exec(
+    function(result) {
+      successCallback(result);
+    },
+    function(error) {
+      errorCallback(error);
+    },
+    'RFIDReaderPlugin',
+    'stop',
+    [options]
+  );
 }
 
 RFIDReaderPlugin.prototype.read = function (successCallback, errorCallback) {
-
-	if (errorCallback == null) {
-        errorCallback = function () {
-        };
-    }
-
-    if (typeof errorCallback != "function") {
-        console.log("RFIDReaderPlugin.read: errorCallback não é uma função");
-        return;
-    }
-
-    if (typeof successCallback != "function") {
-        console.log("RFIDReaderPlugin.read: successCallback não é uma função");
-        return;
-    }
-
-	var options = {};
-
-	exec(
-	     	function(result) {
-	            successCallback(result);
-	        },
-            function(error) {
-               errorCallback(error);
-            },
-
-            'RFIDReaderPlugin',
-            'read',
-			[options]
-     );
+  if (errorCallback == null) {
+    errorCallback = function () {};
+  }
+  if (typeof errorCallback != "function") {
+    console.log("RFIDReaderPlugin.read: errorCallback não é uma função");
+    return;
+  }
+  if (typeof successCallback != "function") {
+    console.log("RFIDReaderPlugin.read: successCallback não é uma função");
+    return;
+  }
+  var options = {};
+  exec(
+    function(result) {
+      successCallback(result);
+    },
+    function(error) {
+      errorCallback(error);
+    },
+    'RFIDReaderPlugin',
+    'read',
+    [options]
+  );
 }
 
 //  var rfidReaderPlugin = new RFIDReaderPlugin();
