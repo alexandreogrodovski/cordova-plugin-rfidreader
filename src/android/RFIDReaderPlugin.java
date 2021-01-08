255e00522f3c28eb139f6761408279bdc130f829
Alexandre Ogrodovski <alexandreogrodovski@gmail.com>
2021-01-08 14:02:58 -0300

    API do leitor

--- src/android/RFIDReaderPlugin.java
+++ src/android/RFIDReaderPlugin.java
@@ -11,6 +11,7 @@ import java.util.Date;
 import java.util.TimeZone;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import com.rscja.deviceapi.RFIDWithUHF;
 
 public class RFIDReaderPlugin extends CordovaPlugin {
 
@@ -21,6 +22,7 @@ public class RFIDReaderPlugin extends CordovaPlugin {
   private CallbackContext callbackContext;
   private JSONArray jsonArray = new JSONArray();
   private boolean execute;
+  private RFIDWithUHF rfidReader;
 
   @Override
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
