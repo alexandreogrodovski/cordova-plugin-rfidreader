9286610f7718b865a7c2519dad26613b1e1c24c4
Alexandre Ogrodovski <alexandreogrodovski@gmail.com>
2021-01-11 14:12:52 -0300

    start and stop

--- src/android/RFIDReaderPlugin.java
+++ src/android/RFIDReaderPlugin.java
@@ -32,7 +32,7 @@ public class RFIDReaderPlugin extends CordovaPlugin {
       switch (action) {
         case START:
           execute = true;
-          //this.rfidReader.startInventoryTag(0, 0);
+          this.rfidReader.startInventoryTag(0, 0);
           cordova
             .getThreadPool()
             .execute(new ReadTask());
@@ -40,7 +40,7 @@ public class RFIDReaderPlugin extends CordovaPlugin {
           break;
         case STOP:
           execute = false;
-          //this.rfidReader.stopInventory();
+          this.rfidReader.stopInventory();
           callbackContext.success();
           break;
         case READ:
