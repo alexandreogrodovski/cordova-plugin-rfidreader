bc8e0d50c330c226f9d2f436f0af1cca4b632d84
Alexandre Ogrodovski <alexandreogrodovski@gmail.com>
2021-01-14 11:22:55 -0300

    configuration exception

--- src/android/RFIDReaderPlugin.java
+++ src/android/RFIDReaderPlugin.java
@@ -17,7 +17,7 @@ public class RFIDReaderPlugin extends CordovaPlugin {
     try {
       //RFIDWithUHF rfidReader = RFIDWithUHF.getInstance();
       //retorno = rfidReader.startInventoryTag(0, 0);
-      throw new com.rscja.deviceapi.exception.ConfigurationException("ConfigurationException");
+      throw new Exception("ConfigurationException");
     } catch (Exception exception) {
       callbackContext.error(exception.getMessage());
     }
